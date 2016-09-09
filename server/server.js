da547181e06ba025cef7dbbf0112d87952c383d9
= <Jmitchell1991@gmail.com>
2016-09-09 13:20:00 -0700

    [server] - sets up heroku port

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 require('./config/middleware')(app, express);
 require('./config/routes')(app, express);
 
-app.listen(port, () => {
-  console.log('\033[34m <TRPPR> Server listening on port: \033[0m', port);
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), () => {
+  console.log('\033[34m <TRPPR> Server listening on port: \033[0m', app.get('port'));
 });
