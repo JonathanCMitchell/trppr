84c8180628e2189712d0e60a86c61992e7e8da11
= <Jmitchell1991@gmail.com>
2016-09-09 13:31:34 -0700

    [server] - makes serverFile es5

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 require('./config/middleware')(app, express);
 require('./config/routes')(app, express);
 
 app.set('port', process.env.PORT || 3000)
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), function() {
   console.log('\033[34m <TRPPR> Server listening on port: \033[0m', app.get('port'));
 });
